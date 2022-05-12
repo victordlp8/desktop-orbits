92b8d8a174273a92107f7417159e4615c2019a9d
victordlp8 <victordlp8@gmail.com>
2022-05-12 21:06:25 +0200

    Polar coords successfully added

--- main.cpp
+++ main.cpp
@@ -6,15 +6,21 @@
 #include <fstream>
 #include <time.h>
 #include <cmath>
+#include "include/polarCoords2.h"
 #include "include/Vec2.h"
 #include "include/json.hpp"
 
 using json = nlohmann::json;
 using namespace std;
 
-long double _pi()
+inline polarCoords2 toPolarCoords(const Vec2 &other)
 {
-    return atan(1) * 4;
+    return polarCoords2(other.module(), other.angle());
+}
+
+inline Vec2 toVec2(const polarCoords2 &other)
+{
+    return Vec2(other.r * cos(other.theta), other.r * sin(other.theta));
 }
 
 bool loadJson(json &json_data, const char *fileName)
@@ -43,8 +49,8 @@ long double PI;
 long double maxVel;
 long double G;      // Gravitational constant
 long double hitBox; // Hit box size
-Vec2 oCoords;
-Vec2 offset;
+polarCoords2 oCoords;
+polarCoords2 offset;
 Vec2 massRange;
 
 const char *cfg_path = "config.json";
@@ -55,12 +61,14 @@ bool loadConfig(const char *cfg_path)
     {
         cout << "Config loaded successfully." << endl;
 
-        PI = _pi();
+        PI = atan(1) * 4;
         maxVel = cfg["maxVel"];
         G = cfg["G_const"];
         hitBox = cfg["hitBox"];
-        oCoords = Vec2(cfg["oCoords"][0], cfg["oCoords"][1]);
-        offset = Vec2(cfg["offset"][0], cfg["offset"][1]);
+        oCoords = toPolarCoords(Vec2(cfg["oCoords"][0], cfg["oCoords"][1]));
+        offset = toPolarCoords(Vec2(cfg["offset"][0], cfg["offset"][1]));
+
+
         massRange = Vec2(cfg["massRange"][0], cfg["massRange"][1]);
 
         return true;
@@ -86,26 +94,26 @@ int random(Vec2 minMax) // range : [min, max]
 class movingEntity
 {
 public:
-    Vec2 pos;
-    Vec2 vel;
+    polarCoords2 pos;
+    polarCoords2 vel;
 
     long double mass; // mass of entity in KB
 
     movingEntity()
     {
-        pos = oCoords + offset;
-        vel = Vec2(random(Vec2(-maxVel, maxVel)), random(Vec2(-maxVel, maxVel)));
+        pos = offset;
+        vel = polarCoords2(random(Vec2(-maxVel, maxVel)), random(Vec2(-maxVel, maxVel)));
         mass = random(massRange);
     }
 
-    movingEntity(Vec2 p)
+    movingEntity(polarCoords2 p)
     {
         pos = p;
-        vel = Vec2(random(Vec2(-maxVel, maxVel)), random(Vec2(-maxVel, maxVel)));
+        vel = polarCoords2(random(Vec2(-maxVel, maxVel)), random(Vec2(-maxVel, maxVel)));
         mass = random(massRange);
     }
 
-    movingEntity(Vec2 p, Vec2 v, long double m)
+    movingEntity(polarCoords2 p, polarCoords2 v, long double m)
     {
         pos = p;
         vel = v;
@@ -119,36 +127,29 @@ public:
         mass = e.mass;
     }
 
-    void setPos(const Vec2 &p)
+    void setPos(const polarCoords2 &p)
     {
         pos = p;
     }
 
-    void setVel(const Vec2 &v)
+    void setVel(const polarCoords2 &v)
     {
         vel = v;
     }
 
-    void push(Vec2 aceleration, long double dt = 1)
+    void push(polarCoords2 aceleration, long double dt = 1)
     {
-        if (aceleration.x != 0)
-        {
-            vel.x += aceleration.x * dt;
-        }
-        if (aceleration.y != 0)
-        {
-            vel.y += aceleration.y * dt;
-        }
+        vel += aceleration * dt;
     }
 
-    long double bounceAngle(const movingEntity &other) const
-    {
-        long double aEntry = vel.angle();
-        long double aBetween = pos.angle(other.pos);
-        long double aExit = PI - aEntry + (PI / 2 - aBetween);
+    // long double bounceAngle(const movingEntity &other) const
+    // {
+    //     long double aEntry = vel.angle();
+    //     long double aBetween = pos.angle(other.pos);
+    //     long double aExit = PI - aEntry + (PI / 2 - aBetween);
 
-        return aExit;
-    }
+    //     return aExit;
+    // }
 
     // void update(HWND hd, long double dt = 1)
     // {
@@ -157,11 +158,12 @@ public:
     // }
 };
 
-Vec2 gravity(movingEntity entity1, movingEntity entity2)
+polarCoords2 gravity(movingEntity entity1, movingEntity entity2)
 {
-    long double fG = G * (entity1.mass * entity2.mass) / pow(entity1.pos.length(entity2.pos), 2);
+    long double fG_r = G * (entity1.mass * entity2.mass) / pow((entity1.pos - entity2.pos).r, 2);
+    long double fG_theta = (entity1.pos - entity2.pos).theta;
 
-    return (entity2.pos - entity1.pos) * fG;
+    return polarCoords2(fG_r, fG_theta);
 }
 
 BOOL CALLBACK MonitorEnumProc(HMONITOR hMonitor,
@@ -173,9 +175,11 @@ BOOL CALLBACK MonitorEnumProc(HMONITOR hMonitor,
     info.cbSize = sizeof(info);
     if (GetMonitorInfo(hMonitor, &info))
     {
-        if (oCoords.x == -1)
+        if (oCoords.r <= 1 && oCoords.r != 0)
         {
-            oCoords = Vec2(abs(info.rcMonitor.left), abs(info.rcMonitor.top));
+            oCoords = toPolarCoords(Vec2(abs(info.rcMonitor.left), abs(info.rcMonitor.top)));
+            oCoords += offset;
+            oCoords += toPolarCoords(Vec2(1920 / 2, 1080 / 2));
         }
     }
 }
@@ -184,22 +188,6 @@ void getOriginCoords()
 {
     EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, 0);
 }
-
-// class desktopIcon
-// {
-// public:
-//     string name;
-//     movingEntity entity;
-//     Vec2 offset;
-
-//     desktopIcon(string n)
-//     {
-//         name = n;
-//         entity = movingEntity();
-//         entity.mass = 1; // Hay que conseguir la masa del icono
-//         offset = Vec2(0, 0);
-//     }
-// };
 
 class desktop
 {
@@ -214,33 +202,14 @@ private:
 public:
     int iconCount;
     vector<movingEntity> icons;
-    // Vec2 desktopSize;
-
-    // desktop(string desktopPath)
-    // {
-    //     for (auto & entry : fs::directory_iterator(desktopPath))
-    //     {
-    //         if (entry.is_regular_file())
-    //         {
-    //             desktopIcon icon(entry.path().filename().string());
-    //             icons.push_back(icon);
-    //         }
-    //     }
-    // }
 
     desktop()
     {
         getOriginCoords();
 
-        // desktopSize = Vec2(GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN));
-        // desktopSize = getDesktopSize();
-
-        // hd = FindWindow("Program Manager", NULL);
         hd = FindWindow("Progman", NULL);
         hd = FindWindowEx(hd, 0, "SHELLDLL_DefView", NULL);
         hd = FindWindowEx(hd, 0, "SysListView32", NULL);
-
-        // hd = GetDesktopWindow();
 
         GetWindowThreadProcessId(hd, &Pi);
         he = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ, false, Pi);
@@ -255,33 +224,26 @@ public:
         {
             ListView_GetItemPosition(hd, i, pC);
             ReadProcessMemory(he, pC, &pt, sizeof(POINT), NULL);
-            icons.push_back(movingEntity(Vec2(offset.x + pt.x, offset.y + pt.y)));
-
-            // cout << pt.x << " " << pt.y << endl;
-            // cout << icons[i].pos.x << " " << icons[i].pos.y << endl;
-            // cout << icons[i].vel.x << " " << icons[i].vel.y << endl;
+            icons.push_back(movingEntity(offset + toPolarCoords(Vec2(pt.x, pt.y))));
         }
     }
 
-    // Vec2 getDesktopSize()
-    // {
-    //     HWND hDesktop;
-    //     RECT rDesktop;
-    //     hDesktop = GetDesktopWindow();
-    //     GetWindowRect(hDesktop, &rDesktop);
-    //     return Vec2(rDesktop.right, rDesktop.bottom);
-    // }
+    void _ListView_SetItemPosition(int i, const polarCoords2 &pos)
+    {
+        Vec2 ListView = toVec2(pos);
+        ListView_SetItemPosition(hd, i, (int)ListView.x, (int)ListView.y);
+    }
 
     void moveIcon(int i)
     {
         icons[i].pos += icons[i].vel;
-        ListView_SetItemPosition(hd, i, (int)icons[i].pos.x - offset.x, (int)icons[i].pos.y - offset.y);
+        _ListView_SetItemPosition(i, icons[i].pos);
     }
 
-    void moveIcon(int i, Vec2 pos)
+    void moveIcon(int i, polarCoords2 p)
     {
-        icons[i].pos = pos;
-        ListView_SetItemPosition(hd, i, (int)icons[i].pos.x - offset.x, (int)icons[i].pos.y - offset.y);
+        icons[i].pos = p;
+        _ListView_SetItemPosition(i, icons[i].pos);
     }
 
     void massIcon(int i, long double m)
@@ -289,7 +251,7 @@ public:
         icons[i].mass = m;
     }
 
-    void velIcon(int i, Vec2 v)
+    void velIcon(int i, polarCoords2 v)
     {
         icons[i].vel = v;
     }
@@ -314,36 +276,40 @@ public:
                 }
                 else if (i != j)
                 {
-                    if (icons[i].pos.length(icons[j].pos) >= 2 * hitBox)
+                    movingEntity entity1 = icons[i];
+                    movingEntity entity2 = icons[j];
+                    if ((entity1.pos - entity2.pos).r >= 2 * hitBox)
                     {
-                        Vec2 aceleration = gravity(icons[i], icons[j]);
-                        icons[i].push(aceleration, dt);
+                        polarCoords2 aceleration = gravity(entity1, entity2);
+                        entity1.push(aceleration, dt);
                     }
                     else
                     {
-                        long double aExit_i = icons[i].bounceAngle(icons[j]);
-                        long double vEntry_i = icons[i].vel.module();
-                        Vec2 vExit_i(vEntry_i * cos(aExit_i), vEntry_i * sin(aExit_i));
-                        icons[i].vel = vExit_i;
+                        // long double aExit_i = icons[i].bounceAngle(icons[j]);
+                        // long double vEntry_i = icons[i].vel.module();
+                        // Vec2 vExit_i(vEntry_i * cos(aExit_i), vEntry_i * sin(aExit_i));
+                        // icons[i].vel = vExit_i;
 
-                        if (true)
-                        {
-                            continue;
+                        // if (true)
+                        // {
+                        //     continue;
 
-                            // long double aExit_j = icons[j].bounceAngle(icons[i]);
-                            // long double vEntry_j = icons[j].vel.module();
-                            // Vec2 vExit_j(vEntry_j * cos(aExit_j), vEntry_j * sin(aExit_j));
-                            // icons[j].vel = vExit_j;
+                        //     // long double aExit_j = icons[j].bounceAngle(icons[i]);
+                        //     // long double vEntry_j = icons[j].vel.module();
+                        //     // Vec2 vExit_j(vEntry_j * cos(aExit_j), vEntry_j * sin(aExit_j));
+                        //     // icons[j].vel = vExit_j;
 
-                            // Vec2 movementQuantity = icons[i].vel * icons[i].mass + icons[j].vel * icons[j].mass;
-                            // icons[i].vel -= movementQuantity / icons[i].mass;
-                            // icons[j].vel -= movementQuantity / icons[j].mass;
-                        }
-                        else
-                        {
-                            icons[i].vel = icons[i].vel * -1;
-                        }
+                        //     // Vec2 movementQuantity = icons[i].vel * icons[i].mass + icons[j].vel * icons[j].mass;
+                        //     // icons[i].vel -= movementQuantity / icons[i].mass;
+                        //     // icons[j].vel -= movementQuantity / icons[j].mass;
+                        // }
+                        // else
+                        // {
+                        //     icons[i].vel = icons[i].vel * -1;
+                        // }
                     }
+                    icons[i] = entity1;
+                    icons[j] = entity2;
                 }
             }
         }
@@ -354,22 +320,22 @@ public:
         VirtualFreeEx(he, pC, 0, MEM_RELEASE);
     }
 
-    void setIconPos(int index, Vec2 p)
+    void setIconPos(int index, polarCoords2 p)
     {
         moveIcon(index, p);
     }
 
-    Vec2 getIconPos(int index)
+    polarCoords2 getIconPos(int index)
     {
         return icons[index].pos;
     }
 
-    void setIconVel(int index, Vec2 v)
+    void setIconVel(int index, polarCoords2 v)
     {
         icons[index].vel = v;
     }
 
-    Vec2 getIconVel(int index)
+    polarCoords2 getIconVel(int index)
     {
         return icons[index].vel;
     }
@@ -385,48 +351,32 @@ int main()
 {
     loadConfig(cfg_path);
 
-    /*POINT pt;
-    ListView_GetItemPosition(hd, 0, pC);
-    ReadProcessMemory(he, pC, &pt, sizeof(POINT), NULL);
-    cout<<"X : "<<pt.x<<" Y : "<<pt.y<<endl;*/
-
     desktop d;
-    // d.addObject(movingEntity());
+
+    cout << "--------------------------------------------" << endl;
     cout << "Icon count: " << d.iconCount << endl;
-    cout << "Coordenadas origin: " << oCoords.x << ", " << oCoords.y << endl;
+    cout << "oCoords: " << oCoords << endl;
+    cout << "offset: " << offset << endl;
+    cout << "--------------------------------------------" << endl;
 
     for (int i = 0; i < d.iconCount; i++)
     {
-        d.moveIcon(i, Vec2((int)(oCoords.x + offset.x + random(Vec2(0, 1920))), (int)(oCoords.y + offset.y + random(Vec2(0, 1080)))));
+        // d.moveIcon(i, oCoords + offset + toPolarCoords(Vec2(random(Vec2(0, 1920)), random(Vec2(0, 1080)))));
+        d.moveIcon(i, oCoords + toPolarCoords(Vec2(random(Vec2(-1920/2, 1920/2)), random(Vec2(-1080/2, 1080/2)))));
+        d.velIcon(i, toPolarCoords(Vec2(random(Vec2(-1000, 1000))/1000, random(Vec2(-1000, 1000))/1000)));
+
         // d.setIconPos(i, Vec2(1920/2 + oCoords.x + offset.x + i * 50, 1080/2 + oCoords.y + offset.y + i * 30));
         // d.setIconPos(i, offset);
     }
     d.update();
 
-    // char c;
-    // cout << "Waiting for a letter to start..." << endl;
-    // cin >> c;
-
-    // d.icons[d.iconCount].mass = range.y * 100;
-    d.moveIcon(0, Vec2((int)(oCoords.x + offset.x + 1920 / 2), (int)(oCoords.y + offset.y + 1080 / 2)));
-    d.massIcon(0, massRange.y / 10);
-
-    // POINT px;
+    d.moveIcon(0, oCoords);
+    d.massIcon(0, massRange.y);
 
     while (true)
     {
-        // GetCursorPos(&px);
-        // d.icons[d.iconCount].setPos(Vec2(px.x, px.y));
-        d.velIcon(0, Vec2(0, 0));
+        d.velIcon(0, toPolarCoords(Vec2(0, 0)));
         d.physics();
-
-        // cout << "Cursor position: " << d.icons[d.iconCount].pos.x << ", " << d.icons[d.iconCount].pos.y << " and mass " << d.icons[d.iconCount].mass << endl;
-
-        // cout << "-----------------------" << endl;
-        // cout << "X: " << cursor.pos.x << " Y: " << cursor.pos.y << endl;
-        // cout << "Position: " << icon.pos.x << " " << icon.pos.y << endl;
-        // cout << "Aceleration: " << aceleration.x << " " << aceleration.y << endl;
-        // cout << "Velocity: " << icon.vel.x << " " << icon.vel.y << endl;
 
         d.update();
 
